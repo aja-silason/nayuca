73ba0a80d8921a34d913c5f3c8e61ab934f81def
ananias augusto <ananiasjaimeaugusto@gmail.com>
2022-12-23 15:38:43 +0100

    Menu Flutuante

---
+++ js/showmenu.js
@@ -0,0 +1,19 @@
+let estadomenu = false;
+
+function abrirMenu(){
+
+    let getFlutuante = document.getElementById('#flutuante');
+
+        if(estadomenu === false){
+            console.log('Teste')
+            /*getFlutuante.style.visibility = "visible";*/
+        }
+}
+
+/*const abrirMenu = document.querySelector('.abrirmenu');
+
+abrirMenu.addEventListener('click', function(e){
+    console.log('Menu flutuante');
+});
+
+*/
